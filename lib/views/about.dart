d51950cabafea3f43c259e1d7c5336ebd8a862e2
legiz-ru <legiz.ru@gmail.com>
2025-07-15 02:15:43 +0300

    Revert "Revert "Fix GitHub Actions build failures: macOsInfo typo and AboutView refactoring""

--- lib/views/about.dart
+++ lib/views/about.dart
@@ -20,8 +20,35 @@ class Contributor {
   });
 }
 
-class AboutView extends StatelessWidget {
+class AboutView extends StatefulWidget {
   const AboutView({super.key});
+
+  @override
+  State<AboutView> createState() => _AboutViewState();
+}
+
+class _AboutViewState extends State<AboutView> {
+  String? _deviceInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadDeviceInfo();
+  }
+
+  Future<void> _loadDeviceInfo() async {
+    try {
+      final systemInfo = system;
+      final version = await systemInfo.version;
+      setState(() {
+        _deviceInfo = 'Device Version: $version';
+      });
+    } catch (e) {
+      setState(() {
+        _deviceInfo = 'Device info unavailable';
+      });
+    }
+  }
 
   _checkUpdate(BuildContext context) async {
     final commonScaffoldState = context.commonScaffoldState;
@@ -144,7 +171,12 @@ class AboutView extends StatelessWidget {
                         Text(
                           globalState.packageInfo.version,
                           style: Theme.of(context).textTheme.labelLarge,
-                        )
+                        ),
+                        if (_deviceInfo != null)
+                          Text(
+                            _deviceInfo!,
+                            style: Theme.of(context).textTheme.bodySmall,
+                          ),
                       ],
                     )
                   ],
